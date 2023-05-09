46fc73bc723439620c6965ff09df472adc8cfae8
Alistair McKinnell <alistairm@nulogy.com>
2023-05-09 07:24:02 -0400

    Address rubocop issues

--- lib/nucop.rb
+++ lib/nucop.rb
@@ -3,9 +3,9 @@ require "nucop/version"
 require "rubocop"
 require "git_diff_parser"
 
-Dir[File.join(__dir__, "nucop/helpers/**/*.rb")].sort.each { |f| require f }
-Dir[File.join(__dir__, "nucop/formatters/**/*.rb")].sort.each { |f| require f }
-Dir[File.join(__dir__, "nucop/cops/**/*.rb")].sort.each { |f| require f }
+Dir[File.join(__dir__, "nucop/helpers/**/*.rb")].each { |f| require f }
+Dir[File.join(__dir__, "nucop/formatters/**/*.rb")].each { |f| require f }
+Dir[File.join(__dir__, "nucop/cops/**/*.rb")].each { |f| require f }
 
 module Nucop
 end
